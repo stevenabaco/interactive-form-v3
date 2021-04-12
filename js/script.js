d07f268c28368beca10595421db4ecf0d43a5215
Steven Abaco <stevenabaco@gmail.com>
2021-04-12 14:01:48 -0500

    Add logic for T-Shirt Section

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 const inputName = document.getElementById('name');
 const selectJobRole = document.getElementById('title');
 const inputOtherJobRole = document.getElementById('other-job-role');
+const selectDesign = document.getElementById('design')
+const selectColor = document.getElementById('color')
+const colorOptions = selectColor.getElementsByTagName('option');
 
-//Set "name" input field focus to true
+//Set form default options
 inputName.focus();
 
-//Only show "Other Job" input if user selects "Other" in the "Job Role" drop down menu.
+//BASIC INFO SECTION
+
+// Hide Other Job Role input until selected by user
 inputOtherJobRole.style.visibility = 'hidden';
 selectJobRole.addEventListener('change', e => {
 	if (e.target.value === 'other') {
@@ -14,3 +19,27 @@ selectJobRole.addEventListener('change', e => {
 		inputOtherJobRole.style.visibility = 'hidden';
 	}
 });
+
+//T-SHIRT SECTION
+
+// Disable color selection until after a Design is selected
+selectColor.disabled = true;
+
+//Add a change event listener to Design selector 
+selectDesign.addEventListener('change', (e) => {
+  //Enable color selection after Design is selected
+  selectColor.disabled = false;
+  //Loop through colors and select only Design specific colors
+  //Auto select a color from the Design specific color for placeholder
+  for (let i = 0; i < colorOptions.length; i++) {
+    if (e.target.value == colorOptions[i].dataset.theme) {
+      colorOptions[i].hidden = false;
+      colorOptions[i].selected = true;
+    } else {
+      colorOptions[i].hidden = true;
+      colorOptions[i].selected = false;
+    }
+  }
+  
+})
+
