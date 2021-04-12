6a9965cf6fa5c598ec0924e5a91322a026f6f3c5
Steven Abaco <stevenabaco@gmail.com>
2021-04-12 11:04:46 -0500

    Add logic to show Other Job input only if Other is selected

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('test')
+const inputName = document.getElementById('name');
+const selectJobRole = document.getElementById('title');
+const inputOtherJobRole = document.getElementById('other-job-role');
+
+//Set "name" input field focus to true
+inputName.focus();
+
+//Only show "Other Job" input if user selects "Other" in the "Job Role" drop down menu.
+inputOtherJobRole.style.visibility = 'hidden';
+selectJobRole.addEventListener('change', e => {
+	if (e.target.value === 'other') {
+		inputOtherJobRole.style.visibility = 'visible';
+	} else {
+		inputOtherJobRole.style.visibility = 'hidden';
+	}
+});
